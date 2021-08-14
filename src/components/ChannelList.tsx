e4733545e1f7df8da4cd2d358a1f2c03d82f84e8
Prithvi-N <prithvin19@gmail.com>
2021-08-14 19:23:50 +0530

    V2

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -11,12 +11,12 @@ export default function ChannelList(props: ListName) {
     // destructure
     const {title,body,id}=props;
     return (
-        <div className="container mx-auto px-10">
+        <div className="container mx-auto px-10 divide-y-4 divide-blue-600 p-0.5 my-4 md:divide-y-4">
             <h2 className=" font-bold text-lg h-2 mb-8">
-            <span className="text-blue-800 mr-1.5">{id}</span>
+            <span className="text-blue-800 mr-1.5">0{id}</span>
             {title}</h2>
-            <div className="divide-y divide-blue-500">
-            <p className="text-gray-400">{body}</p>
+            <div>
+            <div className="text-gray-400 my-4 ">{body}</div>
             </div>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,13 +25,13 @@ export default function Home() {
 
     return (
         <div className ="md:container md:mx-auto">
-            <h1 className="font-bold text-lg h-2 mb-10 text-center m-6 ">Channel Creation</h1>
+            <h1 className="font-bold text-3xl h-2 mb-10 text-center m-6 ">Channel Creation</h1>
              <p className="text-gray-400 text-center mx-10">
              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel ex a nisi ultricies ornare eget auctor magna. Proin consequat, ex a vestibulum varius, mauris eros finibus augue, vel placerat diam quam nec leo. Cras nec ligula lobortis, tristique diam quis, iaculis nibh. Nam ullamcorper laoreet enim. 
 
              </p>
 
-        <div className="grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 m-5 mb-10">
+        <div className="grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-2 m-5 mb-10">
             
               {ListArr.map((item,index)=> 
                <ChannelList key={index}
